ca11749aa8a1660c981779fb5626979c5acf4251
Elijah Busick <92891627+Elijahb22@users.noreply.github.com>
2022-01-27 19:24:35 -0500

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -5,7 +5,7 @@ const db = mysql.createConnection(
       // Your MySQL username,
       user: 'root',
       // Your MySQL password
-      password: '*********',
+      password: 'calofduty1)',
       database: 'election'
     },
     console.log('Connected to the election database.')
